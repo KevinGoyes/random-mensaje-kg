fed2e9a450fcab5d2766cd81b08a039094af5714
Kevin G <kevinfgoyesa@gmail.com>
2024-03-15 20:10:04 +1100

    Creacion de todos los archivos

---
+++ bin/global.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+let random = require("/Users/kevin/Desktop/ProyectosWeb/random-mensaje-kg/src/index");
+
+let random = require("/Users/kevin/Desktop/ProyectosWeb/random-mensaje-kg/bin/global");
+
+random.funnyCommit();
