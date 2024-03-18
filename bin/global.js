336f77974eaa647893e0b3af26aa64953f0210b6
Kevin G <kevinfgoyesa@gmail.com>
2024-03-18 19:35:34 +1100

    .json intalado, global.js y package

--- bin/global.js
+++ bin/global.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 let random = require("/Users/kevin/Desktop/ProyectosWeb/random-mensaje-kg/src/index");
 
-let random = require("/Users/kevin/Desktop/ProyectosWeb/random-mensaje-kg/bin/global");
-
 random.funnyCommit();
